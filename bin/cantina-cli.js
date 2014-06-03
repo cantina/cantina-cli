a4cad2fc74bd102944d3f31146568a4c8c6b81c8
Dan MacTough <danmactough@gmail.com>
2014-06-02 22:25:45 -0400

    Refactor to require init command

--- bin/cantina-cli.js
+++ bin/cantina-cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-var path = require('path')
+var program = require('commander')
+  , pkg = require('../package.json')
+  , path = require('path')
   , cwd = process.cwd()
   , npm = require('npm');
 
@@ -9,35 +11,49 @@ if (cwd === path.resolve(__dirname, '../')) {
   process.exit(65);
 }
 
-var promptForDeps = require('../prompt-for-deps');
-var initPackageJson = require('../init-package-json');
-var copySkel = require('../copy-skel');
+var cmdFound = false;
 
-console.log('');
-// 1. Prompt for desired cantina plugins
-promptForDeps(function (err, results) {
-  var plugins = [];
-  if (err) return die(err);
+program.version(pkg.version);
+program
+  .command('init')
+  .description('Initialize a directory for a new cantina app')
+  .action(init);
+program.parse(process.argv);
+
+cmdFound || program.help();
+
+function init () {
+  cmdFound = true;
+  var promptForDeps = require('../prompt-for-deps');
+  var initPackageJson = require('../init-package-json');
+  var copySkel = require('../copy-skel');
+
   console.log('');
-  if (results.dependencies) {
-    plugins = Object.keys(results.dependencies);
-  }
-  // 2. Create package.json
-  initPackageJson(path.resolve(cwd, 'package.json'), results, function (err, data) {
+  // 1. Prompt for desired cantina plugins
+  promptForDeps(function (err, results) {
+    var plugins = [];
     if (err) return die(err);
-    // No data means we aborted
-    if (!data) die();
-    // 3. Copy skeleton directories
-    copySkel({ main: data.main, plugins: plugins }, function (err) {
+    console.log('');
+    if (results.dependencies) {
+      plugins = Object.keys(results.dependencies);
+    }
+    // 2. Create package.json
+    initPackageJson(path.resolve(cwd, 'package.json'), results, function (err, data) {
       if (err) return die(err);
-      // 4. `npm install`
-      npm.load({}, function (err) {
+      // No data means we aborted
+      if (!data) die();
+      // 3. Copy skeleton directories
+      copySkel({ main: data.main, plugins: plugins }, function (err) {
         if (err) return die(err);
-        npm.commands.install(die);
+        // 4. `npm install`
+        npm.load({}, function (err) {
+          if (err) return die(err);
+          npm.commands.install(die);
+        });
       });
     });
   });
-});
+}
 
 function die (err) {
   if (err) throw err;
